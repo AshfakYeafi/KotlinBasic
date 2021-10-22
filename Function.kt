68c99aad97e5c7970f1f8cdc5bc7e698d5473b6d
AshfakYeafi <ashfakyeafifahim@gmail.com>
2021-10-22 14:29:11 +0600

    update Function.kt

---
+++ Function.kt
@@ -0,0 +1,35 @@
+
+import java.util.*
+
+fun main(args: Array<String>)
+{
+//    add(4,5)
+    var result = max(8,7)
+    println(result)
+}
+
+//fun add(a : Int,b : Int)
+//{
+//    println(a+b)
+//}
+
+//fun add(a : Int,b : Int) : Int
+//{
+//    return a+b
+//}
+
+fun add(a : Int,b : Int) : Int = a+b
+
+//fun max(a: Int,b: Int) : Int
+//{
+//    if(a>b)
+//        return a
+//    else
+//        return b
+//}
+
+fun max(a: Int,b: Int) : Int = if (a>b) a else b
+
+
+
+
