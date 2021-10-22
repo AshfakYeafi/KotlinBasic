6d08aec009051fb3c24e7f730d1080ed51a1d840
AshfakYeafi <ashfakyeafifahim@gmail.com>
2021-10-22 13:32:04 +0600

    create Loop Basic.kt

--- Variable.kt
+++ Variable.kt
@@ -1,3 +1,10 @@
 fun main(){
+    println("Hello World")
 
+    var name:String="fahim"
+    println(name)
+    var age:Int=20
+    println(age)
+
+    println("My name is $name and I'm $age years old")
 }
