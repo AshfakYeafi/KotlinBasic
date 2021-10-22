1f10824afcd220bdb44a3860a891710b7632ba62
AshfakYeafi <ashfakyeafifahim@gmail.com>
2021-10-22 14:22:50 +0600

    update when keyword.kt

--- When Keyowrd.kt
+++ When Keyowrd.kt
@@ -1,3 +1,21 @@
 fun main(){
-
+    /**
+     * when(cond){
+     * case1->{task}
+     * case2->{task2}
+     * else->{default task}
+     * }
+     */
+    var name:String="fah im"
+    when(name){
+        "fahim"->{
+            println("Name is fahim")
+        }
+        "yeafi"->{
+            println("Name is yeafi")
+        }
+        else->{
+            println("This is default block")
+        }
+    }
 }
