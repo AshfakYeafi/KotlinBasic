5e8954d383348bf1660b8a99c67404f9dcef6e1b
AshfakYeafi <ashfakyeafifahim@gmail.com>
2021-10-22 14:24:08 +0600

    update CONDITION.kt

---
+++ Condition.kt
@@ -0,0 +1,23 @@
+fun main(){
+    /**
+     * if(cond){
+     *  task
+     *  }
+     * else if(cond){
+     *  task
+     *  }
+     * else{
+     *  task
+     *  }
+     */
+    var i:Int=5
+    if (i==5){
+        println("I is 5")
+    }
+    else if (i==6){
+        println("I is ^")
+    }
+    else{
+        println("None of them")
+    }
+}
