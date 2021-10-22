ec0a75894aa41325876a47e777c8f68404b506f2
AshfakYeafi <ashfakyeafifahim@gmail.com>
2021-10-22 14:09:41 +0600

    create When Keyword.kt

--- Variable.kt
+++ Variable.kt
@@ -1,10 +1,19 @@
 fun main(){
     println("Hello World")
 
-    var name:String="fahim"
+    var name:String="fahim"     //var variable can be changed future
     println(name)
     var age:Int=20
     println(age)
 
-    println("My name is $name and I'm $age years old")
+    println("My name is $name and I'm $age years old")      // String templating
+
+    var nullVar:String?=null        // this is a null variable which can store null value.
+    println(nullVar)
+
+    val newName:String="yefi"      // You can't change val variable in future, it's like constant
+
+    var isTrue:Boolean=true     // Boolean variable
+
+
 }
